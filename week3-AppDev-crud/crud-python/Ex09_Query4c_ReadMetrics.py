7fd1d2fb74e293b18ce3cd925264610097f1ef12
clunven <cedrick.lunven@gmail.com>
2020-07-15 15:20:28 +0200

    python and fix node

--- week3-AppDev-crud/crud-python/Ex09_Query4c_ReadMetrics.py
+++ week3-AppDev-crud/crud-python/Ex09_Query4c_ReadMetrics.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 from db_connection import Connection
+import uuid
 
-# this is a select statement in python
-connection = Connection()
-output = connection.session.execute("SELECT * FROM killrvideo.user_credentials WHERE email = %s",
-['cv@datastax.com'])
-for row in output:
-    print(row)
-connection.close()
+print('========================================')
+print('Start exercise')
+
+spacecraft_name = 'Crew Dragon Endeavour,SpaceX'
+journey_id      = uuid.UUID('84121060-c66e-11ea-a82e-f931183227ac')
+
+try:
+    connection = Connection()
+    output = connection.session.execute(
+        "select * from spacecraft_speed_over_time where spacecraft_name=%s AND journey_id=%s",
+        [spacecraft_name, journey_id]
+    )
+    offset = 0
+    for row in output:
+       print("idx=", offset, "time=", row.reading_time, "value=", row.speed)
+       offset = offset + 1
+except Exception as e: 
+    print(e)
+    print('Failure')
+else:
+
+    print('Success')
+    print('Closing connection (up to 10s)')
+finally:
+    connection.close()
+print('========================================')
