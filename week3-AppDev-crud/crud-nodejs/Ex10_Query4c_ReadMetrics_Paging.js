4c3878a84f38bf83a0773010b1f4ee679ee97498
clunven <cedrick.lunven@gmail.com>
2020-07-15 12:33:03 +0200

    OK

--- week3-AppDev-crud/crud-nodejs/Ex10_Query4c_ReadMetrics_Paging.js
+++ week3-AppDev-crud/crud-nodejs/Ex10_Query4c_ReadMetrics_Paging.js
@@ -1,29 +1,43 @@
 const connection = require('./db_connection')
+const TimeUuid = require('cassandra-driver').types.TimeUuid;
+
+// Defining our journey
+const journey_id      = TimeUuid.fromString('84121060-c66e-11ea-a82e-f931183227ac')
+const spacecraft_name = 'Crew Dragon Endeavour,SpaceX'
+
+// Page 1
+async function exercice10() {
+ const options1 = { prepare: true , fetchSize: 5 };
+ const queryMetrics = 'select * from spacecraft_speed_over_time where spacecraft_name=? AND journey_id=?'
+ const result1 = await connection.client.execute(queryMetrics, [spacecraft_name, journey_id], options1);
+ console.log("Page1: %i items", result1.rows.length);
+ var offset = 0;
+ result1.rows.forEach(row => {
+ 		offset = offset + 1
+    	console.log("idx:%s, time=%s, value=%s", offset, row.reading_time, row.speed)
+ })
+ let pageState = result1.pageState;
+
+//connection.client.execute(queryMetrics, [spacecraft_name, journey_id], options1)
+//.then(function(result){
+//	console.log("Page1: items", result.rows);
+//    result1.rows.forEach(row => {
+//    	console.log("idx:%s, time=%s, value=%s", offset, row.reading_time, row.speed)
+//    })
+//}
+}
+
+// Page 2
+//let pageState = result.pageState;
+//const options2 = { pageState, prepare: true, fetchSize: 5 };
+//const result2 = connection.client.execute(queryMetrics, [spacecraft_name, journey_id], options1);
+//console.log("Page2: %i items", result2.rows.length);
+//result2.rows.forEach(row => {
+//    	console.log("idx:%s, time=%s, value=%s", offset, row.reading_time, row.speed)
+//    }
+//)
 
 console.log("========================================")
 console.log("Start exercise")
-
-// Defining our journey
-const journey_id      = UUID.fromString('<enter_your_journey_id>')
-const spacecraft_name = 'Crew Dragon Endeavour,SpaceX'
-
-// Get query
-const queryMetrics    = 'select * from spacecraft_speed_over_time where spacecraft_name=? AND journey_id=?'
-
-offset = 0;
-connection.client.execute(queryMetrics, [spacecraft_name, journey_id])
-.then(function(result){
-    result.rows.forEach(row => {
-    	console.log("idx:%s, time=%s, value=%s", offset, row.reading_time, row.speed)
-    	offset = offset + 1
-    })
-   connection.client.shutdown()
-    console.log("SUCCESS")
-})
-.catch(function(error){
-    console.log(error.message)
-    connection.client.shutdown()
-    console.log("FAILED")
-});
-
+exercice10()
 console.log("========================================")
